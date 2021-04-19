3dc460a16be8d115619cf1966b634ce702cec438
AZForest <61096655+AZForest@users.noreply.github.com>
2021-04-19 14:18:02 -0700

    resume fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import classes from './Footer.module.css';
-import resume from '../../assets/AFResume.pdf';
+import resume from '../../assets/Alex Forest - Resume.pdf';
 
 const footer = () => {
     return (
         <div className={classes.Footer}>
             <div className={classes.left}>
                 <a href="https://github.com/AZForest" target="_blank" rel="noopener noreferrer">Github</a>
-                <a href="#">LinkedIn</a>
+                <a href="">LinkedIn</a>
                 <a href={resume} download>Resume</a>
             </div>
             <div className={classes.right}>
